91c59f1c94bfbbfadf2fabba4a27e8637b690e08
thyago_lbt <thyago14@outlook.fr>
2019-11-05 10:11:08 +0100

    fin

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('#green').mouseover(function() {
+    $('#text').css('color', 'green')
+  })
+  $('#red').mouseover(function() {
+    $('#text').css('color', 'red')
+  })
+  $('#blue').mouseover(function() {
+    $('#text').css('color', 'blue')
+  })
+})
